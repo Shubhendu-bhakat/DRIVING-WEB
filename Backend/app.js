ba249db89e5469c1edba36ef88ee3e8f2a5b3443
Shubhendu Bhakat <kumarsubho2003@gmail.com>
2025-02-12 23:17:50 +0530

    connection to Db done using mongoose 12/02/25 11:17

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const app = express();
 
-const cors = require('cors');
+const cors = require('cors'); //make sure to install the cors package and dotenv package
 app.use(cors());//cross domain request handler
-
+//db connection 
+const connectToDB = require('./db/db');
+connectToDB();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
