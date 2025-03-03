1551a7cd9ddb3007e07eeba9fce4fcf4ec58d878
Shubhendu Bhakat <kumarsubho2003@gmail.com>
2025-03-03 23:27:25 +0530

    added blacklitToken model for logout

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -59,6 +59,13 @@ module.exports.loginUser = async (req, res, next) => {
 
     const token = user.generateAuthToken();
 
-
+    res.cookie('token', token);
     res.status(200).json({ token, user });
+}
+
+//get profile 
+module.exports.getUserProfile = async (req, res, next) => {
+
+    res.status(200).json(req.user);
+
 }
